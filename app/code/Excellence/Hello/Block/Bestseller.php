8a7e228a1202f811e9d6994c60d097d59bb9dd20
Sachin Kumar Yadav <emb-sachyad@emb-ban-415.embitel.local>
2018-06-21 13:00:27 +0530

    prodict by id and sku

---
+++ app/code/Excellence/Hello/Block/Bestseller.php
@@ -0,0 +1,24 @@
+<?php
+namespace Excellence\Hello\Block;
+  
+class Bestseller extends \Magento\Framework\View\Element\Template
+{ 
+    protected $_productRepository;
+  
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context, 
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        array $data = []
+    ) {
+        $this->_productRepository = $productRepository;
+        parent::__construct($context, $data);
+    }
+  
+    public function getProductById($id) {
+        return $this->_productRepository->getById($id);
+    }
+  
+    public function getProductBySku($sku) {
+        return $this->_productRepository->get($sku);
+    }
+}
